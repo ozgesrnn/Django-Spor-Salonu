c16be39be425404b80962f0f6f26fd964ca2dee0
ozgesrnn@gmail.com <ozgesrnn@gmail.com>
2021-03-31 18:36:29 +0300

    Hakkımızda Sayfası Oluşturuldu

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')), #herhangi bir şey yazmadanda çalışması için
+    path('aboutus/',views.aboutus, name='aboutus'),
+    path('contact/', views.contact, name='contact'),
+    path('references/', views.references, name='references'),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
 if settings.DEBUG: #new
     urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
