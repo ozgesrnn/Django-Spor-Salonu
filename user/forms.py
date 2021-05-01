7023c3db7712d3eaf89ece32aa09f2b857d3b026
ozgesrnn@gmail.com <ozgesrnn@gmail.com>
2021-05-01 17:57:58 +0300

    Kullanıcı Üye bilgilerini güncelleme

---
+++ user/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django.forms import TextInput
+
+from home.models import  UserProfile
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username' , 'email', 'first_name', 'last_name')
+        widgets = {
+            'username'  : TextInput(attrs={'class' : 'input' , 'placeorder' : 'username'}),
+            'email'     : TextInput(attrs={'class': 'input', 'placeorder': 'email'}),
+            'first_name': TextInput(attrs={'class': 'input', 'placeorder': 'first_name'}),
+            'last_name' : TextInput(attrs={'class': 'input', 'placeorder': 'last_name'}),
+
+        }
+
+CITY = [
+    ('Istanbul ', 'Istanbul '),
+    ('Ankara ', 'Ankara '),
+    ('Izmir ', 'Izmir '),
+
+]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('phone', 'address' , 'city', 'country', 'image')
+        widgets = {
+            'phone': TextInput(attrs={'class': 'input', 'placeorder': 'phone'}),
+            'address': TextInput(attrs={'class': 'input', 'placeorder': 'address'}),
+            'city': TextInput(attrs={'class': 'input', 'placeorder': 'city'}, choices = CITY),
+            'country': TextInput(attrs={'class': 'input', 'placeorder': 'country'}),
+            'image': TextInput(attrs={'class': 'input', 'placeorder': 'image',}),
+
+        }
