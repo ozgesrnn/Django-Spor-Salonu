0b4f6bf51b3f41e3f298027a40c403de4fd57deb
Özge Şirin <58072302+ozgesrnn@users.noreply.github.com>
2021-06-23 13:33:06 +0300

    Düzenlemeler yapıldı

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'mptt',
+
 ]
 
 MIDDLEWARE = [
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    path('update/' , views.user_update, name='user_update'),
-    path('password/' , views.change_password, name='change_password'),
-    path('comments/' , views.comments, name='comments'),
-    path('deletecomment/<int:id>' , views.deletecomment, name='deletecomment'),
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.change_password, name='change_password'),
+    path('comments/', views.comments, name='comments'),
+    path('deletecomment/<int:id>', views.deletecomment, name='deletecomment'),
     #path('addcomment/<int:id>', views.addcomment, name = 'addcomment')
 
 
