d1ffedcd5b98421e0fbf197c82dd960c1a8f9b80
Özge Şirin <58072302+ozgesrnn@users.noreply.github.com>
2021-06-28 14:21:00 +0300

    Üyelik tipleri için model oluşturuldu.

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_products, name='category_products'),
     path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('paket/<int:id>/<slug:slug>/', views.paket_detail, name='paket_detail'),
     path('search/', views.product_search, name='product_search'),
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', views.login_view, name='login_view'),
