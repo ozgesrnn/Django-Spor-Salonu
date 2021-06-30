b5c7f229dbef0d90e22d9388faf886dad4896cf7
Özge Şirin <58072302+ozgesrnn@users.noreply.github.com>
2021-07-01 01:22:21 +0300

    Order işlemleri

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from home import views
+from order import views as orderviews
 
 urlpatterns = [
     path('', include('home.urls')), #herhangi bir şey yazmadanda çalışması için
@@ -39,6 +40,7 @@ urlpatterns = [
     path('join/', views.join_view, name='join_view'),
     path('faq/', views.faq, name='faq'),
     path('paket/', views.paket, name='paket'),
+    path('plancart/', orderviews.plancart, name='plancart'),
 
 
 ]
